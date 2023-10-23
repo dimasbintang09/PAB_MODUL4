c3be8ae3d229ce418d69da3da3b1fff2050272ce
Dimas Bintang <117976515+dimasbintang09@users.noreply.github.com>
2023-10-23 21:22:49 +0700

    second commit

--- screens/NativeBaseBasic.js
+++ screens/NativeBaseBasic.js
@@ -1,3 +1,18 @@
+// import React from "react";
+// import { NativeBaseProvider, Center, Heading } from "native-base";
+
+// const NativeBaseBasic = () => {
+//   return (
+//     <NativeBaseProvider>
+//       <Center flex={1}>
+//         <Heading>Native Base</Heading>
+//       </Center>
+//     </NativeBaseProvider>
+//   );
+// };
+
+// export default NativeBaseBasic;
+
 import React from "react";
 import { NativeBaseProvider, Center, Heading } from "native-base";
 
